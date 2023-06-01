a35745ac26279d3bb3f7eee1bea293dde57d9352
gbugliani <guilherme.bugliani@refit.com.br>
2023-06-01 15:00:14 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { PopoverComponent } from './popover/popover.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -6,11 +7,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { TaskService } from './services/task.service'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PopoverComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, TaskService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
+import { TaskService } from './../services/task.service';
 import { Component } from '@angular/core';
+import { AlertController, PopoverController, ToastController } from '@ionic/angular';
+import { PopoverComponent } from '../popover/popover.component';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,123 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  type : string = "pending";
+
+  constructor
+  (
+    public alertController: AlertController,
+    public taskService: TaskService,
+    public toastController: ToastController,
+    public popoverController: PopoverController
+  ) {}
+
+  async presentAlertPromptAdd() {
+    const alert = await this.alertController.create({
+      header: 'Adicionar Tarefa!',
+      inputs: [
+        {
+          name: 'task',
+          type: 'text',
+          placeholder: 'Tarefa'
+        },
+        {
+          name: 'date',
+          type: 'date',
+          min: '2020-01-01',
+          max: '2025-01-01'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        },{
+          text: 'Salvar',
+          handler: (alertData) => {
+            if (alertData.task != "")
+            this.taskService.addTask(alertData.task, alertData.date);
+            else {
+              this.presentToast();
+              this.presentAlertPromptAdd();
+            }
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  async presentAlertPromptDelete(index: number) {
+    const alert = await this.alertController.create({
+      header: 'Excluir tarefa!',
+      message: 'Deseja realmente excluir a tarefe?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        },{
+          text: 'Excluir',
+          handler: () => this.taskService.delTask(index)
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  async presentAlertPromptUpdate(index: number, task: { date: any; value: any; }) {
+    const alert = await this.alertController.create({
+      header: 'Atualizar Tarefa!',
+      inputs: [
+        {
+          name: 'task',
+          type: 'text',
+          placeholder: 'Tarefa',
+          value : task.value
+        },
+        {
+          name: 'date',
+          type: 'date',
+          min: '2020-01-01',
+          max: '2025-01-01',
+          value: task.date.getFullYear() + "-" + ((task.date.getMonth()+1) < 10 ? "0" + task.date.getMonth()+1 : task.date.
+          getMonth()+1) + "-" + ((task.date.getDay()+1) < 10 ? "0" + task.date.getDay() : task.date.getDay())
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        },{
+          text: 'Salvar',
+          handler: (alertData) => {
+            if (alertData.task != "")
+            this.taskService.updateTask(index, alertData.task, alertData.date);
+            else {
+              this.presentToast();
+              this.presentAlertPromptAdd();
+            }
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: "Preencha a tarefa!",
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  async presentPopOver(ev : any){
+    const popover = await this.popoverController.create({
+      component: PopoverComponent,
+      event: ev,
+      translucent: true
+    });
+    return await popover.present();
+  }
 
 }
